b9b9a13cef388b4f7078e2b8a199d5327097085c
douwepausma <douwepausma@gmail.com>
2025-06-20 17:16:07 +0200

    add: public crud api routes

--- server/src/routes/contentApi/index.ts
+++ server/src/routes/contentApi/index.ts
@@ -7,4 +7,44 @@ export default [
       policies: [],
     },
   },
+  {
+    method: 'GET',
+    path: '/:id',
+    handler: 'redirectsController.findOne',
+    config: {
+      policies: [],
+    },
+  },
+  {
+    method: 'POST',
+    path: '/',
+    handler: 'redirectsController.create',
+    config: {
+      policies: [],
+    },
+  },
+  {
+    method: 'PUT',
+    path: '/:id',
+    handler: 'redirectsController.update',
+    config: {
+      policies: [],
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/:documentId',
+    handler: 'redirectsController.delete',
+    config: {
+      policies: [],
+    },
+  },
+  {
+    method: 'POST',
+    path: '/import',
+    handler: 'redirectsController.import',
+    config: {
+      policies: [],
+    },
+  }
 ];
